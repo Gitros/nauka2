614c2b8ea17b0fb271fc0cef9847ef9997437237
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-25 15:17:09 +0100

    zad1

---
+++ zad1.js
@@ -0,0 +1,9 @@
+/*
+1. Napisz program, który wyswietli na ekranie Twoje imie i nazwisko oraz
+date urodzenia
+*/
+
+const name = 'jakub';
+const nazwisko = 'owczarek';
+const dataUrodzenia = '2003-09-18';
+document.write(name + nazwisko + dataUrodzenia);
