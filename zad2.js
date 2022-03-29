46cb01b91389c09178728e0a774cd39cc325d750
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-29 20:41:16 +0200

    zad 2

--- zad2.js
+++ zad2.js
@@ -2,11 +2,20 @@
 2. Napisz program, sprawdzajacy czy dany rok jest przestepny.
 */
 
-let rok;
-let check;
-if (rok % 4 === 0 && rok % 100 !== 0) {
-  alert('Twój rok jest przystępny');
+let rokInput = document.getElementById('rok-input');
+let checkBtn = document.getElementById('check-btn');
+let wynikOut = document.getElementById('wynik');
+
+function sprawdz() {
+  if (rokInput.value % 4 === 0 && rokInput.value % 100 !== 0) {
+    return 'Twój rok jest przystępny';
+  } else {
+    return 'Twój rok nie jest przystępny';
+  }
 }
-if (rok % 4 !== 0 && rok % 100 === 0) {
-  alert('Twój rok nie jest przystępny');
-}
+
+const buttonClick = () => {
+  wynik.textContent = sprawdz();
+};
+
+checkBtn.addEventListener('click', buttonClick);
