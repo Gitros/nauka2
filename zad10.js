2cea6d912f806ef334594a8e4df91fbd26f42584
Gitros <31899790+Gitros@users.noreply.github.com>
2022-04-05 23:32:03 +0200

    zad 10 part1

---
+++ zad10.js
@@ -0,0 +1,11 @@
+/*Napisz program, który wczyta z klawiatury 4 liczby rzeczywiste,
+ a nastepnie wyswietli informacje ile z nich jest mniejszych od 0. */
+
+ const liczbaInput = document.getElementById('liczba-input')
+ const obliczBtn = document.getElementById('oblicz-btn')
+ const wynik = document.getElementById('wynik');
+
+ function oblicz() {
+     let result = parseFloat(liczbaInput.value) 
+     if result < 0, i++;
+ }
