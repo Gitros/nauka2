bd4f5026a1a521a8d87076994c826bd400f22d35
Gitros <31899790+Gitros@users.noreply.github.com>
2022-04-05 23:18:47 +0200

    zad 8

---
+++ zad8.js
@@ -0,0 +1,28 @@
+/*  Napisz program, wczyta dlugosci boków prostokata i i w zaleznosci od
+wyboru uzytkownika obliczy jego obwód lub pole.
+*/
+
+const liczbaInput = document.getElementById('liczba-input');
+const liczbaInput2 = document.getElementById('liczba-input2');
+const obwodBtn = document.getElementById('obwod-btn');
+const poleBtn = document.getElementById('pole-btn');
+const wynik = document.getElementById('wynik');
+
+function obwod(){
+    let result = parseInt(liczbaInput.value) + parseInt(liczbaInput2.value);
+    return result * 2
+}
+function pole(){
+    let result = parseInt(liczbaInput.value) * parseInt(liczbaInput2.value);
+    return result
+}
+
+const buttonClickObwod = () => {
+    wynik.textContent = obwod();
+  };
+  obwodBtn.addEventListener('click', buttonClickObwod);
+
+const buttonClickPole = () => {
+    wynik.textContent = pole();
+  };
+  poleBtn.addEventListener('click', buttonClickPole);
