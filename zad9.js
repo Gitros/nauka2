13e719519f49d714fddd92818c6512773e259570
Gitros <31899790+Gitros@users.noreply.github.com>
2022-04-05 23:20:24 +0200

    zad 9

---
+++ zad9.js
@@ -0,0 +1,26 @@
+const liczbaInput = document.getElementById('liczba-input');
+const obwodBtn = document.getElementById('obwod-btn');
+const poleBtn = document.getElementById('pole-btn');
+const wynik = document.getElementById('wynik');
+
+function pole(){
+    let result =  3.14 * Math.pow(parseInt(liczbaInput.value), 2) ;
+    return result 
+}
+
+function obwod(){
+  let result = 2 * 3.14 * parseInt(liczbaInput.value);
+  return result 
+}
+
+
+
+const buttonClickObwod = () => {
+    wynik.textContent = obwod();
+  };
+  obwodBtn.addEventListener('click', buttonClickObwod);
+
+const buttonClickPole = () => {
+    wynik.textContent = pole();
+  };
+  poleBtn.addEventListener('click', buttonClickPole);
